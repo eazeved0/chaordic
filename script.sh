a4d7464829271e435c33c839a0ee9692f255d067
eazeved0 <eazevedo@bsd.com.br>
2016-06-29 14:12:23 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,6 +13,6 @@ PROC=`nproc`
 #!/bin/bash
 for (( c=1; c<=$PROC; c++ ))
 do
-echo "serve('0.0.0.0', 900$c);" >> /root/clusterv4.js
+echo "serve('0.0.0.0', 900$c);" >> /root/chaordic/clusterv4.js
 echo "server web$c 127.0.0.1:900$c check" >> /etc/haproxy/haproxy.cfg 
 done
